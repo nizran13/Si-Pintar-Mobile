97406d3a82ddf4392dd566613a669af29e6ff8c7
nizran13 <nizranpaputungan@gmail.com>
2025-12-19 11:42:03 +0800

    Create Code.js

---
+++ 01-Sistem-Login/Code.js
@@ -0,0 +1,36 @@
+// Nama Sheet harus tepat (Case-Sensitive)
+var SHEET_NAME = "Users";
+
+function doGet(e) {
+  return HtmlService.createTemplateFromFile('Index').evaluate()
+    .setTitle("Si-Pintar Login")
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+// Fungsi utama untuk validasi login
+function checkLogin(username, password) {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName(SHEET_NAME);
+  var data = sheet.getDataRange().getValues();
+  
+  for (var i = 1; i < data.length; i++) {
+    if (data[i][0] == username && data[i][1] == password) {
+      return {
+        status: "success",
+        nama: data[i][2],
+        url: data[i][3] // URL Spreadsheet B (Dashboard)
+      };
+    }
+  }
+  return { status: "failed", message: "Username atau Password Salah!" };
+}
+
+// Fungsi agar script bisa dipanggil oleh Aplikasi Android (API Mode)
+function doPost(e) {
+  var action = e.parameter.action;
+  if (action == "login") {
+    var res = checkLogin(e.parameter.username, e.parameter.password);
+    return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(ContentService.MimeType.JSON);
+  }
+}
